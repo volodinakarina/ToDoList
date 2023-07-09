823cd5c51a4ffe494ddc3c154220c2923c65e9af
karinavolodina <volodinakarina27@yandex.ru>
2023-07-09 12:44:50 +0300

    change

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from django.contrib.auth.models import Group
+
 from core.models import User
 
 
@@ -11,14 +11,17 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         ("Personal info", {"fields": ("first_name", "last_name", "email")}),
-        ("Permissions", {
-            "fields": (
-                "is_active",
-                "is_staff",
-            ),
-        },
-         ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
+
 
 admin.site.unregister(Group)
